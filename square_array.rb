01090c5f4bfc17d56a8fdc5d226b4b0686cc92a0
Stephen Thurman <thurman024@gmail.com>
2018-01-24 19:36:06 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,11 @@
+# def square_array(array)
+#   new_numbers = []
+#   array.each do |num|
+#     n = num**2
+#     new_numbers.push(n)
+#   end
+#   new_numbers
+# end
 def square_array(array)
-  new_numbers = []
-  array.each do |num|
-    n = num**2
-    new_numbers.push(n)
-  end
-  new_numbers
+  array.collect { |x| x**2 }
 end
